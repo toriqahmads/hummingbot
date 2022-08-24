59eec38d2e9a87787feb4a14173fc61cd475b125
baron737 <sambaran@hummingbot.io>
2022-08-24 14:29:53 +0530

    Updated userstream data source test cases

--- test/hummingbot/connector/exchange/bittrex/test_bittrex_api_user_stream_data_source.py
+++ test/hummingbot/connector/exchange/bittrex/test_bittrex_api_user_stream_data_source.py
@@ -1,16 +1,11 @@
 import asyncio
-import json
 import unittest
 from typing import Awaitable, Optional
 from unittest.mock import AsyncMock, MagicMock, patch
 
-from hummingbot.connector.exchange.bittrex import bittrex_constants as CONSTANTS
 from hummingbot.connector.exchange.bittrex.bittrex_api_user_stream_data_source import BittrexAPIUserStreamDataSource
 from hummingbot.connector.exchange.bittrex.bittrex_auth import BittrexAuth
-from hummingbot.connector.exchange.bittrex.bittrex_web_utils import build_api_factory
-from hummingbot.connector.test_support.network_mocking_assistant import NetworkMockingAssistant
 from hummingbot.connector.time_synchronizer import TimeSynchronizer
-from hummingbot.core.api_throttler.async_throttler import AsyncThrottler
 
 
 class BittrexAPIUserStreamDataSourceTest(unittest.TestCase):
@@ -22,17 +17,11 @@ class BittrexAPIUserStreamDataSourceTest(unittest.TestCase):
         cls.ev_loop = asyncio.get_event_loop()
         cls.api_key = "someKey"
         cls.secret_key = "someSecret"
-        cls.base_asset = "COINALPHA"
-        cls.quote_asset = "HBOT"
-        cls.trading_pair = f"{cls.base_asset}-{cls.quote_asset}"
-        cls.symbol = f"{cls.base_asset}{cls.quote_asset}"
 
     def setUp(self) -> None:
         super().setUp()
         self.listening_task: Optional[asyncio.Task] = None
         self.log_records = []
-        self.mocking_assistant = NetworkMockingAssistant()
-        self.throttler = AsyncThrottler(rate_limits=CONSTANTS.RATE_LIMITS)
         self.mock_time_provider = MagicMock()
         self.mock_time_provider.time.return_value = 1000
         self.time_synchronizer = TimeSynchronizer()
@@ -41,8 +30,6 @@ class BittrexAPIUserStreamDataSourceTest(unittest.TestCase):
             auth=BittrexAuth(api_key=self.api_key,
                              secret_key= self.secret_key,
                              time_provider=self.mock_time_provider),
-            connector=AsyncMock(),
-            api_factory=build_api_factory()
         )
         self.resume_test_event = asyncio.Event()
         self.data_source.logger().setLevel(1)
@@ -66,126 +53,44 @@ class BittrexAPIUserStreamDataSourceTest(unittest.TestCase):
         self.resume_test_event.set()
         raise exception
 
-    def _create_return_value_and_unlock_test_with_event(self, value):
+    def _unlock_test_with_event(self):
         self.resume_test_event.set()
-        return value
+        return
 
     def async_run_with_timeout(self, coroutine: Awaitable, timeout: float = 1):
         ret = self.ev_loop.run_until_complete(asyncio.wait_for(coroutine, timeout))
         return ret
 
-    @patch("aiohttp.ClientSession.ws_connect", new_callable=AsyncMock)
-    def test_listen_for_user_stream_subscribes_to_orders_and_balances_events(self, ws_connect_mock):
-        ws_connect_mock.return_value = self.mocking_assistant.create_websocket_mock()
-        success_auth = {
-            "R": {
-                "Success": True,
-                "ErrorCode": None
-            },
-            "I": 1
-        }
-        success_response = {
-            "R": [
-                {
-                    "Success": True,
-                    "ErrorCode": None
-                },
-                {
-                    "Success": True,
-                    "ErrorCode": None
-                },
-                {
-                    "Success": True,
-                    "ErrorCode": None
-                },
-                {
-                    "Success": True,
-                    "ErrorCode": None
-                },
-            ],
-            "I": 1
-        }
-        self.mocking_assistant.add_websocket_aiohttp_message(
-            websocket_mock=ws_connect_mock.return_value,
-            message=json.dumps(success_auth))
-        self.mocking_assistant.add_websocket_aiohttp_message(
-            websocket_mock=ws_connect_mock.return_value,
-            message=json.dumps(success_response))
+    @patch("signalr_aio.Connection")
+    @patch("hummingbot.connector.exchange.bittrex.bittrex_api_user_stream_data_source.BittrexAPIUserStreamDataSource."
+           "authenticate_client", new_callable=AsyncMock)
+    def test_for_succesful_connect_to_signalr_and_register_hub(self, auth_mock, ws_connect_mock):
+        conn_mock = MagicMock()
+        ws_connect_mock.return_value = conn_mock
+        msgeventhook_mock, errorevent_mock = MagicMock(), MagicMock()
+        msgeventhook_mock.__iadd__.return_value = msgeventhook_mock
+        errorevent_mock.__iadd__.return_value = errorevent_mock
+        conn_mock.received = msgeventhook_mock
+        conn_mock.error = errorevent_mock
+        auth_mock.side_effect = self._unlock_test_with_event()
+        self.listening_task = self.ev_loop.create_task(self.data_source._connected_websocket_assistant())
+        self.async_run_with_timeout(self.resume_test_event.wait())
+        conn_mock.register_hub.assert_called_with("c3")
+        msgeventhook_mock.__iadd__.assert_called_with(self.data_source._handle_message)
+        msgeventhook_mock.__iadd__.assert_called_once()
+        errorevent_mock.__iadd__.assert_called_with(self.data_source._handle_error)
+        errorevent_mock.__iadd__.assert_called_once()
 
-        output_queue = asyncio.Queue()
-
-        self.listening_task = self.ev_loop.create_task(self.data_source.listen_for_user_stream(output=output_queue))
-
-        self.mocking_assistant.run_until_all_aiohttp_messages_delivered(ws_connect_mock.return_value)
-
-        sent_messages = self.mocking_assistant.json_messages_sent_through_websocket(
-            websocket_mock=ws_connect_mock.return_value)
-
-        self.assertEqual(2, len(sent_messages))
-        expected_sub_msg = {
-            "H": "c3",
-            "M": "Subscribe",
-            "A": [["balance", "order", "heartbeat", "execution"]],
-            "I": 1
-        }
-        self.assertEqual(expected_sub_msg, sent_messages[1])
-        self.assertTrue(self._is_logged(
-            "INFO",
-            "Successfully authenticated to user stream..."
-        ))
-
-    @patch("aiohttp.ClientSession.ws_connect", new_callable=AsyncMock)
-    def test_listen_for_user_stream_authentication_failure(self, ws_connect_mock):
-        ws_connect_mock.return_value = self.mocking_assistant.create_websocket_mock()
-        err_resp = {"R": {
-            "Success": False,
-            "ErrorCode": "INVALID_APIKEY"
-        },
-            "I": 1
-        }
-        self.mocking_assistant.add_websocket_aiohttp_message(
-            websocket_mock=ws_connect_mock.return_value,
-            message=json.dumps(err_resp))
-
-        output_queue = asyncio.Queue()
-        self.listening_task = self.ev_loop.create_task(self.data_source.listen_for_user_stream(output=output_queue))
-
-        self.mocking_assistant.run_until_all_aiohttp_messages_delivered(ws_connect_mock.return_value)
-
-        self.assertTrue(self._is_logged(
-            "ERROR",
-            "Error occurred authenticating websocket connection.."
-        ))
-
-    @patch("aiohttp.ClientSession.ws_connect", new_callable=AsyncMock)
-    def test_listen_for_user_stream_does_not_queue_empty_payload(self, mock_ws):
-        mock_ws.return_value = self.mocking_assistant.create_websocket_mock()
-        sucess_auth = {
-            "R": {
-                "Success": True,
-                "ErrorCode": None
-            },
-            "I": 1
-        }
-        self.mocking_assistant.add_websocket_aiohttp_message(
-            mock_ws.return_value,
-            json.dumps(sucess_auth))
-        self.mocking_assistant.add_websocket_aiohttp_message(mock_ws.return_value, "")
-
-        msg_queue = asyncio.Queue()
-        self.listening_task = self.ev_loop.create_task(
-            self.data_source.listen_for_user_stream(msg_queue)
-        )
-
-        self.mocking_assistant.run_until_all_aiohttp_messages_delivered(mock_ws.return_value)
-
-        self.assertEqual(0, msg_queue.qsize())
-
-    @patch("aiohttp.ClientSession.ws_connect", new_callable=AsyncMock)
-    def test_listen_for_user_stream_connection_failed(self, mock_ws):
-        mock_ws.side_effect = lambda *arg, **kwars: self._create_exception_and_unlock_test_with_event(
+    @patch("signalr_aio.Connection")
+    @patch("hummingbot.connector.exchange.bittrex.bittrex_api_user_stream_data_source.BittrexAPIUserStreamDataSource."
+           "authenticate_client", new_callable=AsyncMock)
+    @patch("hummingbot.connector.exchange.bittrex.bittrex_api_user_stream_data_source.BittrexAPIUserStreamDataSource."
+           "_invoke", new_callable=AsyncMock)
+    def test_for_authentication_failed(self, invoke_mock, auth_mock, ws_connect_mock):
+        conn_mock = MagicMock()
+        ws_connect_mock.return_value = conn_mock
+        auth_mock.side_effect = lambda *arg, **kwars: self._create_exception_and_unlock_test_with_event(
             Exception("TEST ERROR."))
-
         msg_queue = asyncio.Queue()
         self.listening_task = self.ev_loop.create_task(
             self.data_source.listen_for_user_stream(msg_queue)
@@ -196,3 +101,73 @@ class BittrexAPIUserStreamDataSourceTest(unittest.TestCase):
         self.assertTrue(
             self._is_logged("ERROR",
                             "Unexpected error while listening to user stream. Retrying after 5 seconds..."))
+
+    @patch("signalr_aio.Connection")
+    @patch("hummingbot.connector.exchange.bittrex.bittrex_api_user_stream_data_source.BittrexAPIUserStreamDataSource."
+           "authenticate_client", new_callable=AsyncMock)
+    @patch("hummingbot.connector.exchange.bittrex.bittrex_api_user_stream_data_source.BittrexAPIUserStreamDataSource."
+           "_invoke", new_callable=AsyncMock)
+    def test_listen_for_user_stream_subscribes_to_orders_and_balances_events(self, invoke_mock, auth_mock, ws_connect_mock):
+        conn_mock = MagicMock()
+        conn_mock.register_hub.return_value = MagicMock()
+        ws_connect_mock.return_value = conn_mock
+        auth_mock.side_effect = self._unlock_test_with_event()
+        sub_channels = ["order", "balance", "execution"]
+        invoke_mock.return_value = [
+            {"Success": True, "ErrorCode": None},
+            {"Success": True, "ErrorCode": None},
+            {"Success": True, "ErrorCode": None}
+        ]
+        output_queue = asyncio.Queue()
+        self.listening_task = self.ev_loop.create_task(self.data_source.listen_for_user_stream(output=output_queue))
+        self.async_run_with_timeout(self.resume_test_event.wait())
+        invoke_mock.assert_called_with("Subscribe", [sub_channels])
+        self.assertTrue(self._is_logged(
+            "INFO",
+            "Subscribed to private channels"
+        ))
+
+    @patch("signalr_aio.Connection")
+    @patch("hummingbot.connector.exchange.bittrex.bittrex_api_user_stream_data_source.BittrexAPIUserStreamDataSource."
+           "authenticate_client", new_callable=AsyncMock)
+    @patch("hummingbot.connector.exchange.bittrex.bittrex_api_user_stream_data_source.BittrexAPIUserStreamDataSource."
+           "_invoke", new_callable=AsyncMock)
+    def test_listen_for_user_stream_fails_to_subscribe_to_channels(self, invoke_mock, auth_mock, ws_connect_mock):
+        conn_mock = MagicMock()
+        conn_mock.register_hub.return_value = MagicMock()
+        ws_connect_mock.return_value = conn_mock
+        auth_mock.side_effect = self._unlock_test_with_event()
+        invoke_mock.return_value = [
+            {"Success": False, "ErrorCode": "Test-error-code"},
+            {"Success": True, "ErrorCode": None},
+            {"Success": True, "ErrorCode": None}
+        ]
+        msg_queue = asyncio.Queue()
+        self.listening_task = self.ev_loop.create_task(
+            self.data_source.listen_for_user_stream(msg_queue)
+        )
+        self.async_run_with_timeout(self.resume_test_event.wait())
+        self.assertTrue(
+            self._is_logged("ERROR",
+                            "Subscription to 'order' failed: Test-error-code"))
+
+    @patch("signalr_aio.Connection")
+    @patch("hummingbot.connector.exchange.bittrex.bittrex_api_user_stream_data_source.BittrexAPIUserStreamDataSource."
+           "authenticate_client", new_callable=AsyncMock)
+    @patch("hummingbot.connector.exchange.bittrex.bittrex_api_user_stream_data_source.BittrexAPIUserStreamDataSource."
+           "_invoke", new_callable=AsyncMock)
+    def test_listen_for_user_stream_raises_exception_while_subscribing(self, invoke_mock, auth_mock, ws_connect_mock):
+        conn_mock = MagicMock()
+        conn_mock.register_hub.return_value = MagicMock()
+        ws_connect_mock.return_value = conn_mock
+        auth_mock.side_effect = self._unlock_test_with_event()
+        invoke_mock.side_effect = lambda *arg, **kwars: self._create_exception_and_unlock_test_with_event(
+            Exception("TEST ERROR."))
+        msg_queue = asyncio.Queue()
+        self.listening_task = self.ev_loop.create_task(
+            self.data_source.listen_for_user_stream(msg_queue)
+        )
+        self.async_run_with_timeout(self.resume_test_event.wait())
+        self.assertTrue(
+            self._is_logged("ERROR",
+                            "Failed to subscribe to all private channels"))
